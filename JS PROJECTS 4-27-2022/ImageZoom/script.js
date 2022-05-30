daa853f06fe21114535ac105abe5638f1b2b1621
mrsaad2129@gmail.com <mrsaad2129@gmail.com>
2022-05-30 13:54:49 +0500

    Portfolio Completed

--- JS PROJECTS 4-27-2022/ImageZoom/script.js
+++ JS PROJECTS 4-27-2022/ImageZoom/script.js
@@ -5,9 +5,6 @@ img.addEventListener("mousemove", (e) => {
 	// console.log("hey");
 	let x = e.clientX - e.target.offsetLeft;
 	let y = e.clientY - e.target.offsetTop;
-	console.log(x);
-	console.log(y);
-
 	img.style.transformOrigin = `${x}px ${y}px`;
 	img.style.transform = "scale(2)";
 });
--- Saad.Portfolio/main.js
+++ Saad.Portfolio/main.js
@@ -1,23 +1,10 @@
 let closeBtn = document.querySelector(".closeBtn");
 let navigation = document.querySelector(".navigation");
 let menuBtn = document.querySelector(".menuBtn");
-
-// closeBtn.onclick = () => {
-// 	menuBtn.style.display = "block";
-// 	// navigation.style.height = "0";
-// };
-menuBtn.onclick = () => {
-	if (navigation.style.bottom == "0%") {
-		menuBtn.src = "../icons/menu-4-fill.svg";
-		navigation.style.bottom = "-100%";
-	} else {
-		navigation.style.bottom = "0%";
-		menuBtn.src = "../icons/close-fill.svg";
-	}
-	// menuBtn.style.display = "none";
-	// navigation.style.height = "0";
-};
-
+let Email = document.getElementById("email");
+let Name = document.getElementById("name");
+let PhoneNum = document.getElementById("phone");
+let Description = document.getElementById("desc");
 let n;
 let sendBtn = document.querySelector(".send");
 let modal = document.querySelector(".modal");
@@ -28,6 +15,110 @@ let email = document.querySelector("#email");
 let modeBtn = document.querySelector(".modeBtn");
 let inps = Array.from(document.querySelectorAll(".inp"));
 let em, na, num;
+let BarmenuBtn = document.querySelector(".BarmenuBtn");
+let navItems = Array.from(document.querySelectorAll(".nav-item"));
+let dt = new Date().getHours();
+let barmode = document.querySelector(".barmode");
+
+// Menu
+menuBtn.onclick = () => {
+	// let literal = document.querySelector(".literal");
+	// document.body.classList.add("literal");
+	if (navigation.style.width == "16.5rem") {
+		navigation.style.width = "6rem";
+		document.body.style.paddingLeft = "6rem";
+		menuBtn.style.left = "6.5rem";
+		menuBtn.src = "../icons/menu-4-fill.svg";
+		navItems.forEach((navItem) => {
+			navItem.style.display = "none";
+		});
+		document.querySelector(".Profile").style.display = "none";
+	} else {
+		navItems.forEach((navItem) => {
+			navItem.style.display = "flex";
+		});
+		menuBtn.src = "../icons/close-fill.svg";
+		menuBtn.style.left = "90%";
+		document.querySelector(".Profile").style.display = "block";
+		navigation.style.width = "16.5rem";
+	}
+};
+window.onscroll = () => {
+	if (navigation.style.width == "16.5rem") {
+		navigation.style.width = "6rem";
+		document.body.style.paddingLeft = "6rem";
+		menuBtn.style.left = "6.5rem";
+		menuBtn.src = "../icons/menu-4-fill.svg";
+		navItems.forEach((navItem) => {
+			navItem.style.display = "none";
+		});
+		document.querySelector(".Profile").style.display = "none";
+		BarmenuBtn.src = "./icons/menu-4-fill.svg";
+		navigation.style.bottom = "-100%";
+	}
+};
+
+// For Width 400 & less than 400
+
+BarmenuBtn.onclick = () => {
+	// navigation.classList.toggle('mob')
+	if (navigation.style.bottom == "5.5%") {
+		BarmenuBtn.src = "./icons/menu-4-fill.svg";
+		navigation.style.bottom = "-100%";
+		navigation.style.width = "101vw";
+		navItems.forEach((navItem) => {
+			navItem.style.display = "none";
+		});
+	} else {
+		navigation.style.bottom = "5.5%";
+		navigation.style.width = "101vw";
+		BarmenuBtn.src = "./icons/close-fill.svg";
+		navItems.forEach((navItem) => {
+			navItem.style.display = "block";
+		});
+	}
+};
+// Dark Mode Settings
+
+barmode.addEventListener("click", () => {
+	document.body.classList.toggle("DarkMode");
+	menuBtn.classList.toggle("inverted");
+
+	if (localStorage.getItem("theme") == "dark") {
+		localStorage.setItem("theme", "light");
+	} else {
+		localStorage.setItem("theme", "dark");
+	}
+});
+
+if (localStorage.getItem("theme") == "dark") {
+	document.body.classList.remove("DarkMode");
+	BarmenuBtn.classList.remove("inverted");
+	menuBtn.classList.remove("inverted");
+} else if (localStorage.getItem("theme") == "light") {
+	document.body.classList.add("DarkMode");
+	BarmenuBtn.classList.add("inverted");
+	menuBtn.classList.add("inverted");
+} else {
+	localStorage.setItem("theme", "light");
+	BarmenuBtn.classList.remove("inverted");
+	menuBtn.classList.remove("inverted");
+}
+// For Width more than 400
+modeBtn.addEventListener("click", () => {
+	document.body.classList.toggle("DarkMode");
+	// menuBtn.classList.toggle("inverted");
+	// BarmenuBtn.classList.toggle("inverted");
+	if (localStorage.getItem("theme") == "dark") {
+		localStorage.setItem("theme", "light");
+		menuBtn.classList.add("inverted");
+	} else {
+		localStorage.setItem("theme", "dark");
+		menuBtn.classList.remove("inverted");
+	}
+});
+// Dark Mode Settings End Here;
+// For Form inputs
 NameMain.addEventListener("change", (e) => {
 	n = e.target.value;
 	if (e.target.value != "") {
@@ -39,7 +130,7 @@ email.addEventListener("change", (e) => {
 		em = true;
 	}
 });
-number.addEventListener("change", (e) => {
+phone.addEventListener("change", (e) => {
 	if (e.target.value != "") {
 		num = true;
 	}
@@ -50,21 +141,16 @@ sendBtn.addEventListener("click", () => {
 	}
 	NameContainer.innerText = n;
 });
-
-modeBtn.addEventListener("click", () => {
-	if (document.body.style.background == "#fff") {
-		// document.body.style.background = "#000";
-		document.body.style.background = "#000";
-		document.body.style.color = "#fff";
-		inps.forEach((inp) => {
-			inp.style.background = "#141414";
-		});
-		// 	//
-	} else {
-		document.body.style.background = "#fff";
-		document.body.style.color = "#000";
-		inps.forEach((inp) => {
-			inp.style.background = "#efefef";
-		});
-	}
-});
+setTimeout(() => {
+	modal.style.display = "none";
+}, 2000);
+// Contact Form Data To Gmail
+// & it's Reseting
+sendBtn.onclick = () => {
+	setTimeout(() => {
+		Email.value = "";
+		Name.value = "";
+		PhoneNum.value = "";
+		Description.value = "";
+	}, 300);
+};
